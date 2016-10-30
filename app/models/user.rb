2b8f0d46e5a2034745d57e120eaad32b678507c6
Manuel <mail@mbaeurle.com>
2016-10-30 17:08:03 +0100

    Add unsecure password setter

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,4 +11,17 @@ class User < ActiveRecord::Base
   def confirmed?
     !! confirmed_at
   end
+
+  # implement unsecure, custom password setter
+  def password=(unencrypted_password)
+    if unencrypted_password.nil?
+      self.password_digest = nil
+    elsif !unencrypted_password.empty?
+      self.password_plain = unencrypted_password
+      self.password_md5 = Digest::MD5.hexdigest(unencrypted_password)
+      @password = unencrypted_password
+      cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
+      self.password_digest = BCrypt::Password.create(unencrypted_password, cost: cost)
+    end
+  end
 end
